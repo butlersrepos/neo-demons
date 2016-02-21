b2984e5d9760f5fc69346f286e37171b6ce0d9e5
Eric Butler <ericbutler000@gmail.com>
2016-02-21 18:41:06 -0500

    Improved npm tasks. Added sass folder. Added main include for starting CSS

--- src/neo-demons.js
+++ src/neo-demons.js
@@ -1,21 +1,24 @@
 (() => {
 	"use strict";
-	let koa = require('koa');
 	let views = require('koa-views');
+	let serve = require('koa-static');
+	let app = require('koa')();
 
-	let app = koa();
-
-	app.use(views(`${__dirname}/../views`, {
+	// Serve all build artifacts as static resources
+	app.use(serve(`./build`));
+	// Allows "yield this.render('myJadeFile')" throughout routes
+	app.use(views(`./views`, {
 		extension: 'jade',
 		map      : {
 			jade: 'jade'
 		}
 	}));
+	// Catch-all requests and redirect to index.jade for now
 	app.use(function *() {
 		yield this.render('index');
 	});
-
+	// Start up server on 8080
 	app.listen(8080, () => {
-		console.log(`Server is up at http://localhost:8080`);
+		console.log(`Server is totally up at http://localhost:8080`);
 	});
 })();
